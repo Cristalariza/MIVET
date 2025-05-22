15db1bea7f7accf6a94ea710c7b61d7f02ec9e40
Cristal Ariza <cristalarizamartinez@gmail.com>
2025-05-22 17:12:52 -0500

    configuración de rutas y validaciones de autenticación

---
+++ admin_veterinaria/src/pages/products.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h1>ESTOY EN EL CONTENIDO PRODUCTS.VUE</h1>
+    </div>
+</template>
+<script setup>
+definePage({
+  meta: {
+    not_authenticate: false,
+  },
+})
+</script>
